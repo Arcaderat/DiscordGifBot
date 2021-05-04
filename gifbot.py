82b68ca0d0e43adcd550a9ef9c0d90a7dd33a33c
Parker <pkswanson1@outlook.com>
2021-05-03 20:43:30 -0400

    TikTok embeds

--- gifbot.py
+++ gifbot.py
@@ -1,6 +1,7 @@
 import re
 import json
 import discord
+import requests
 
 
 client = discord.Client()
@@ -19,7 +20,7 @@ async def on_message(message):
     #ignore own messages
     if message.author == client.user:
         return
-    print("message received")
+    #print("message received")
     #print(message.content)
     #print(message.attachments)
     #set shortcut to attachment
@@ -35,11 +36,30 @@ async def on_message(message):
         await message.channel.send('Shortcut saved, type ' + shortcut + ' to send:')
         await message.channel.send(commands[shortcut])
 
+    #commands start and end with ;
     if message.content.startswith(';') and message.content.endswith(';'):
         if message.content in commands.keys():
             await message.channel.send(message.author.display_name)
             await message.channel.send(commands[message.content])
             await message.delete()
         print(message.content)
+
+    #check if message is a tiktok link
+    if message.content.startswith('https://vm.tiktok.com/'):
+        sent_url = message.content
+        #the shortened mobile url doesn't work for the api, so get the full url
+        real_url = requests.get(sent_url).url
+        #request to tiktok api
+        response = requests.get('https://www.tiktok.com/oembed?url=' + real_url)
+        if response.status_code == 200:
+            video_info = response.json()
+            video_description = video_info["title"]
+            video_title = video_info["author_name"] + " on Tiktok"
+            video_thumbnail = video_info["thumbnail_url"]
+            video_embed = discord.Embed(title=video_title, description=video_description, url=sent_url,
+                                        name="TikTok", color=0x1a1a1a)
+            video_embed.set_thumbnail(url=video_thumbnail)
+            await message.channel.send(embed=video_embed)
+
 
 client.run("ODMyMzU3NzkzOTg0OTM4MDM0.YHinmg.19XKG_UtbRUDt4GfLB2aQUkicNs")
